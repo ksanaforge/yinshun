4defdb70890b16455ceb68215bdfad123fc249e4
yapcheahshen <yapcheahshen@gmail.com>
2017-02-25 14:55:45 +0800

    set corpora in html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const main=require("accelon2017");
-const corpora=["yinshun","taisho","taixu"];
-main({corpora});
+const corpora=window.corpora;
+if (!corpora) {
+	const rootele=document.getElementById("root");
+	rootele.innerHTML="<h1>&nbsp;:( System failure</h1><br>&nbsp;Missing window.corpora.";
+	rootele.style="margin:0px;background-color:blue;color:white;height:100%"
+} else {
+	main({corpora});	
+}
