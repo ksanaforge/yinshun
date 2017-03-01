1749bcccc9580956aafdf062aa1ebb9eab10f9d2
yapcheahshen <yapcheahshen@gmail.com>
2017-03-01 18:41:20 +0800

    mpps as stand alone cor

---
+++ config.js
@@ -0,0 +1,7 @@
+	window.accelon2017={
+		corpora:["yinshun","mpps","taisho","taixu"],
+		sites:[
+	{name:"一行",url:"http://buddhaspace.org/dict/index.php?keyword=$$"},
+	{name:"康熙",url:"http://kangxi.adcs.org.tw/kangxizidian/#$$",max:1}
+	]
+	}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const main=require("accelon2017");
-const corpora=window.accelon2017&&window.accelon2017.corpora;
-console.log(window.accelon2017)
-if (!corpora) {
-	const rootele=document.getElementById("root");
-	rootele.innerHTML="<h1>&nbsp;:( System failure</h1><br>&nbsp;Missing window.corpora.";
-	rootele.style="margin:0px;background-color:blue;color:white;height:100%"
-} else {
-	main({corpora});	
-}
+setTimeout(function(){
+	const corpora=window.accelon2017&&window.accelon2017.corpora;
+	if (!corpora) {
+		const rootele=document.getElementById("root");
+		rootele.innerHTML="<h1>&nbsp;:( System failure</h1><br>&nbsp;Missing window.corpora.";
+		rootele.style="margin:0px;background-color:blue;color:white;height:100%"
+	} else {
+		main({corpora});	
+	}
+});
